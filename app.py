5c58fa17c4e397b0c3579c0c19ed6e5465eadca0
Enrico Sobrinho Gregório <enricogregorio5@gmail.com>
2022-02-13 17:30:52 -0300

    update page eventos

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def showPageEventos():
             else:
                 return pageEventos + '<script>window.alert("Esse título já foi usado em outro evento!")</script>'
     else:
-        return redirectMaster
+        return redirect('http://127.0.0.1:5000/login')
 
 @app.route('/alunos', methods=['GET', 'POST'])
 def showPageAlunos():
